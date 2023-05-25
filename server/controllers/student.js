e2659e85f18796dfb0f39dbbde08b1b3b4f59fdc
Salma <you@example.com>
2023-05-25 06:30:39 +0200

    Added

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -56,10 +56,60 @@ export const showFiles = async(req, res)=>{
       }
 };
 
+
 export const showAvailableCourses = async (req, res) => {
-    res.send("availableCourses");
+    const studentId = "200327";
+
+  try {
+    const student = await studentModel.findOne({ user_code: studentId }).lean();
+    const enrolledCourses = student.courses_id || [];
+
+    const eligibleCourses = await courseModel.find({requre_course: { $in: enrolledCourses.concat("None") }}).lean();
+    
+    const successfulCourses = await sucessCoursesModel.find({student_id: studentId}).lean();
+    
+      const passedCourses = successfulCourses.map(({ course_id }) => course_id).flat();
+    //   console.log(passedCourses)
+      const allCourses = await courseModel.find().lean();
+      const availableCourses = allCourses.filter(({ courses_id }) => 
+
+        !passedCourses.includes(courses_id) && !enrolledCourses.includes(courses_id)
+      );
+      res.render("studentTemplates/studentEnrollCourse", { courses: availableCourses });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: "Failed to retrieve available courses",
+        error: error.message
+      });
+    }
 };
 
 export const enrollCourse = async (req, res) => {
-    
+    try {
+        const studentId = "200327"; 
+        const student = await studentModel.findOne({ user_code: studentId });
+        
+        
+        const selectedCourses = req.body.course_code; // array of course IDs selected by the student
+        const enrolledCourses = student.courses_id || []; // array of course IDs the student is already enrolled in
+        const newCourses = selectedCourses.filter((courseId) => !enrolledCourses.includes(courseId)); // filter out courses the student is already enrolled in
+        
+        const minCourses = 4; // minimum number of courses a student can enroll in
+        if (student.courses_id.length + newCourses.length < minCourses) {
+          throw new Error(`You must enroll in at least ${minCourses} courses.`);
+        }
+        const maxCourses = 6; // maximum number of courses a student can enroll in
+        if (student.courses_id.length + newCourses.length > maxCourses) {
+          throw new Error(`You can only enroll in a maximum of ${maxCourses} courses.`);
+        }
+
+        student.courses_id = enrolledCourses.concat(newCourses); // add new course IDs to the student's courses_id array
+        await student.save(); // save the updated student document
+        res.redirect('/student');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+      }
 };
