b0133275b2682790a3c175473073d7d50534a4b4
said-ahmd <said.ahmd.cs@gmail.com>
2023-05-25 05:42:41 +0300

    student fetch

--- server/controllers/loginAuth.js
+++ server/controllers/loginAuth.js
@@ -1,29 +1,25 @@
 import courses_model from "../database/models/courses_model.js"
+import student_model from "../database/models/student_model.js";
 export let findCourses;
 export const loginAuth = async (req, res) => {
   try {
     const userType = req.userType;
-    const doctor_id =req.body.user_code;
+    const user_code =req.body.user_code;
+
     console.log(userType);
     //check the user type
     if (userType == 'admin') {
       return res.render("adminTemplates/adminHomePage");
     } else if (userType == 'student') {
+        const user_code =req.body.user_code;
+        const student=await student_model.find({user_code}).lean();
+        console.log(student);        
       return res.render("studentTemplates/studentHomePage");
-    } else {  
-        const findID = await courses_model.findOne( {doctor_id} ).lean();
-    
-        const courses=await courses_model.find({doctor_id}).lean();
-        console.log(courses)
-    
-    
-        if(!findID){
-            return res.status(404).send("errorMessag");
-        }
-        else{
-            
-          return res.render('doctorTemplates/doctorHomePage', { courses }); 
-        }
+    } 
+    else {  
+        const courses=await courses_model.find({doctor_id: user_code}).lean();
+        console.log(courses);        
+        return res.render('doctorTemplates/doctorHomePage', { courses }); 
   }
   
   } catch (error) {
@@ -31,3 +27,5 @@ export const loginAuth = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
+
+
